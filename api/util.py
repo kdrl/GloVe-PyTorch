2ea16a063c630edf56f3d87054b4451639b3914f
kdrl <kdrl7@naver.com>
2017-08-15 23:29:38 +0900

    Update: calculating now. will evaluate model soon.

--- api/util.py
+++ api/util.py
@@ -10,19 +10,3 @@ def most_similar(word_embeddings_array, word_to_index, index_to_word, word, resu
             data.append((index_to_word[i],cosine_similarity([word_embeddings_array[target_index]],[word_embeddings_array[i]])[0][0]))
     data.sort(key=lambda tup: tup[1], reverse=True)
     return data[:result_num]
-
-class LoggerClass():
-    def __init__(self, logfilepath):
-        super(LoggerClass, self).__init__()
-        self.logger = logging.getLogger('mylogger')
-        fomatter = logging.Formatter('[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s')
-        fileHandler = logging.FileHandler(logfilepath)
-        streamHandler = logging.StreamHandler()
-        fileHandler.setFormatter(fomatter)
-        streamHandler.setFormatter(fomatter)
-        self.logger.addHandler(fileHandler)
-        self.logger.addHandler(streamHandler)
-        self.logger.setLevel(logging.DEBUG)
-        
-    def print_and_log(str):
-        self.logger.info(str)
--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import os
+import logging
+import logging.handlers
 import datetime
 import numpy as np
 import torch
 from api.model import GloVeClass
 from api.process import load_txt_and_tokenize
-from api.util import LoggerClass
 
-logger = LoggerClass("./model/20170814.log")
+LOGFILE_PATH = "./model/20170814.log"
+logger = logging.getLogger('mylogger')
+fomatter = logging.Formatter('[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s')
+fileHandler = logging.FileHandler(LOGFILE_PATH)
+streamHandler = logging.StreamHandler()
+fileHandler.setFormatter(fomatter)
+streamHandler.setFormatter(fomatter)
+logger.addHandler(fileHandler)
+logger.addHandler(streamHandler)
+logger.setLevel(logging.INFO)
 
-logger.print_and_log("[GPU setting]")
+logger.info("[GPU setting]")
 os.environ["LD_LIBRARY_PATH"] = "/usr/local/cuda/lib64:/usr/local/lib:/usr/lib64"
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3,4"
-logger.print_and_log("Done")
+logger.info("Done")
 
-logger.print_and_log("[CUDA check]")
+logger.info("[CUDA check]")
 CUDA_AVAILABLE = torch.cuda.is_available()
-logger.print_and_log("CUDA availability : ", CUDA_AVAILABLE)
-logger.print_and_log("Done")
+logger.info("CUDA availability : {}".format(CUDA_AVAILABLE))
+logger.info("Done")
 
 torch.manual_seed(1)
 if CUDA_AVAILABLE:
     torch.cuda.manual_seed(1)
 
-logger.print_and_log("[Parameter setting]")
+logger.info("[Parameter setting]")
 CONTEXT_SIZE = 8
 EMBED_SIZE = 300
 X_MAX = 100
@@ -33,46 +43,46 @@ L_RATE = 0.05
 NUM_EPOCHS = 10000
 TOTAL_PROCESS_NUM = 16
 LOWER_BOUND_OCCURENCE = 50
-logger.print_and_log(
+logger.info(
     "CONTEXT_SIZE = {}\nEMBED_SIZE = {}\nX_MAX = {}\nALPHA = {}\nBATCH_SIZE = {}\nL_RATE = {}\nNUM_EPOCHS = {}\nTOTAL_PROCESS_NUM = {}\nLOWER_BOUND_OCCURENCE = {}"
     .format(CONTEXT_SIZE, EMBED_SIZE, X_MAX, ALPHA, BATCH_SIZE, L_RATE, NUM_EPOCHS, TOTAL_PROCESS_NUM, LOWER_BOUND_OCCURENCE)
 )
-logger.print_and_log("Done")
+logger.info("Done")
 
-logger.print_and_log("[Load training data]")
+logger.info("[Load training data]")
 CORPUS_PATH = ["/data/pearl_hdd1/fukui/dataset/corpus/text8/text8"]
 tokenized_corpus = load_txt_and_tokenize(CORPUS_PATH, LOWER_BOUND_OCCURENCE)
 unique_word_list = np.unique(tokenized_corpus)
 unique_word_list_size = unique_word_list.size
-logger.print_and_log("TOKENIZED_CORPUS_SIZE : ", len(load_txt_and_tokenize))
-logger.print_and_log("UNIQUE_WORD_SIZE : ", unique_word_list.size)
-logger.print_and_log("Done")
+logger.info("TOKENIZED_CORPUS_SIZE : {}".format(len(tokenized_corpus)))
+logger.info("UNIQUE_WORD_SIZE : {}".format(unique_word_list.size))
+logger.info("Done")
 
-logger.print_and_log("[Load model]")
+logger.info("[Load model]")
 GloVe = GloVeClass(tokenized_corpus, unique_word_list, EMBED_SIZE, CONTEXT_SIZE, X_MAX, ALPHA, TOTAL_PROCESS_NUM)
 if CUDA_AVAILABLE:
-    logger.print_and_log("with GPU")
+    logger.info("with GPU")
     # GloVe = GloVe.cuda() # one gpu
     GloVe = torch.nn.DataParallel(GloVe, device_ids=[0, 1, 2, 3]).cuda()
-logger.print_and_log("Done")
+logger.info("Done")
 
-logger.print_and_log("[Model parameters]")
+logger.info("[Model parameters]")
 for p in GloVe.parameters():
-    logger.print_and_log(p.size())
-logger.print_and_log("Done")
+    logger.info(p.size())
+logger.info("Done")
 
-logger.print_and_log("[Set optimizer]")
+logger.info("[Set optimizer]")
 # optimizer = optim.Adam(GloVe.parameters(), L_RATE)
 optimizer = torch.optim.Adagrad(GloVe.parameters(), L_RATE)
-logger.print_and_log("Done")
+logger.info("Done")
 
-logger.print_and_log("[Start training]")
+logger.info("[Start training]")
 list_for_estimation = list()
-start_loop_for_estimation = 5
+start_loop_for_estimation = 10
 end_loop_for_estimation = 20
 
 for epoch in range(NUM_EPOCHS):
-    logger.print_and_log("EPOCH {} start".format(epoch + 1))
+    logger.info("EPOCH {} start".format(epoch + 1))
     losses = []
     update_time = ((unique_word_list_size * unique_word_list_size) // BATCH_SIZE)
     point = int(update_time / 5)
@@ -86,10 +96,10 @@ for epoch in range(NUM_EPOCHS):
                 list_for_estimation.append(datetime.datetime.now() - time)
                 time = datetime.datetime.now()
             elif (i == end_loop_for_estimation):
-                logger.print_and_log("Estimated calculation time per each epoch is {}".format(str((np.mean(list_for_estimation)) * update_time)))
+                logger.info("Estimated calculation time per each epoch is {}".format(str((np.mean(list_for_estimation)) * update_time)))
 
         if i % point == 0:
-            logger.print_and_log("{} % done".format(int(i / update_time * 100)))
+            logger.info("{} % done".format(int(i / update_time * 100)))
         word_u_variable, word_v_variable, words_co_occurences, words_weights = GloVe.module.next_batch(BATCH_SIZE)
         # word_u_variable, word_v_variable, words_co_occurences, words_weights = GloVe.next_batch(self.batch_size) # when it is not in parallel
         forward_output = GloVe(word_u_variable, word_v_variable)
@@ -98,11 +108,11 @@ for epoch in range(NUM_EPOCHS):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    logger.print_and_log("Train Epoch: {} \t Loss: {:.6f}".format(epoch + 1, np.mean(losses)))
+    logger.info("Train Epoch: {} \t Loss: {:.6f}".format(epoch + 1, np.mean(losses)))
     np.savez(
         'model/glove.npz', 
         word_embeddings_array=GloVe.module.embedding(), 
         word_to_index=GloVe.module.word_to_index,
         index_to_word=GloVe.module.index_to_word
         )
-logger.print_and_log("Done")
+logger.info("Done")
